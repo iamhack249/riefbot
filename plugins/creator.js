d14c8830142b2cec6f73d55f868dfd0a6323b9ea
zxcode <77441608+Arief-zx@users.noreply.github.com>
2023-05-23 12:54:29 +0700

    Add files via upload

---
+++ plugins/creator.js
@@ -0,0 +1,32 @@
+let fetch = require('node-fetch')
+let fs = require('fs')
+let handler = async (m, { conn, usedPrefix, text, args, command }) => {
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let name = await conn.getName(who)
+// FAKE KONTAK
+ const fcon = {
+	 key:
+	 { fromMe: false,
+	 participant: `0@s.whatsapp.net`, ...(m.chat ? 
+	 { remoteJid: "status@broadcast" } : {}) },
+	 message: { "contactMessage": { "title":"sri","h": `haloo`, 'jpegThumbnail': fs.readFileSync('./thumbnail.jpg')}}
+	}
+
+if (command == 'creator') {
+ let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp; RiefBotz\nNICKNAME:👑 Developer\nORG: Riefzz\nTITLE:soft\nitem1.TEL;waid=6282172824438:+62 821 7282 4438\nitem1.X-ABLabel:Contact Owner\nitem2.URL:https://wa.me/+62821172824438\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET: iamhack249@gmail.com\nitem3.X-ABLabel:Email\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:Lokasi\nBDAY;value=date:📮30 May 2004\nEND:VCARD`
+const tag_own = await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fcon })
+let caption = `👋 Hai *@${who.split("@")[0]}*, Nih owner aku chat aja gk usah malu.`
+    await conn.sendButton(m.chat, caption, wm, null, [['Menu', '.menu']], m, { quoted: fcon, mentions: conn.parseMention(caption) })
+}
+if (command == 'owner') {
+  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;${author};;;\nFN:${author}\nORG:${author}\nTITLE:\nitem1.TEL;waid=6282172824438:+62 821 7282 4438\nitem1.X-ABLabel:${author}\nX-WA-BIZ-DESCRIPTION:Note: chat aja gk usah malu\nX-WA-BIZ-NAME:${author}\nEND:VCARD`
+await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fcon })
+await conn.reply(m.chat, `Halo kak @${m.sender.split(`@`)[0]} itu nomor ownerku, chat aja gk usah malu`,m)
+  }
+}
+handler.help = ['owner', 'creator']
+handler.tags = ['info']
+
+handler.command = /^(creator|owner)$/i
+
+module.exports = handler
